6db8e977b846cee8bc8ff641502c2e5c9fd7a482
Hassan Ashas <f180128@nu.edu.pk>
2022-08-01 02:58:50 +0500

    added pagination

--- expenses/exp/views.py
+++ expenses/exp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Category, Expense
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 def index(request):
     
     categories = Category.objects.all()
     expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 2)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
     context = {
-        'cateogries': categories,
-        'expenses': expenses
+        'expenses': expenses,
+        'page_obj': page_obj,
     }
     return render(request, 'expenses/index.html', context)
 
